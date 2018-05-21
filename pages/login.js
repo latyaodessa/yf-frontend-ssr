44ca7ed47bb3b5b47f0740faca42f8839c5a9994
Andrii Fedorenko <a1349252@unet.univie.ac.at>
2018-05-21 19:26:41 +0200

    login page

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import MainLayoutEmpty from '../src/components/layouts/MainLayoutEmpty'
-
+import MainLayoutWithNavigation from '../src/components/layouts/MainLayoutWithNavigation'
+import FaceBookLoginButton from '../src/components/user/login/facebook-login';
+import VKLoginButton from '../src/components/user/login/vk-login';
+import Redirector from '../src/components/user/login/redirector';
+import styles from '../res/styles/user/login.scss'
 
 class Index extends React.Component {
     static async getInitialProps({store, isServer}) {
@@ -12,14 +15,25 @@ class Index extends React.Component {
 
     render() {
         return (
-            <MainLayoutEmpty>
-                    <div className="login-form">
-                      <img src="/static/img/brand/youngfolks-logo.jpg" className="login-profile-img"/>
-                      <div className="profile-container">
-
-                      </div>
+            <MainLayoutWithNavigation>
+                <style jsx>{styles}</style>
+                <div className="child-container">
+                    <div className="wrapper">
+                        <div className="content-wrapper">
+                            <div className="content">
+                                <div className="login-form">
+                                    <img src="/static/img/brand/youngfolks-logo.jpg" className="login-profile-img"/>
+                                    <div className="profile-container">
+                                        <Redirector/>
+                                        <VKLoginButton/>
+                                        <FaceBookLoginButton/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-            </MainLayoutEmpty>
+                </div>
+            </MainLayoutWithNavigation>
         )
     }
 }
