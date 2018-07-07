4fd1a1070adf92d27dc4d41f1a4c80f13c574a98
Andrii Fedorenko <a1349252@unet.univie.ac.at>
2018-07-07 15:57:00 +0200

    wrong HOST again :(

--- frontend/src/constants/abstract-rest-client.js
+++ frontend/src/constants/abstract-rest-client.js
@@ -1,5 +1,5 @@
-export const HOST = () =>  'http://localhost:8080/';
-// export const HOST = () => {
-//     let backend = typeof window === 'undefined' ? 'http://backend:8080/' : window.location.protocol + "//" + window.location.hostname +  "/";
-//     return backend;
-// };
+// export const HOST = () =>  'http://localhost:8080/';
+export const HOST = () => {
+    let backend = typeof window === 'undefined' ? 'http://backend:8080/' : window.location.protocol + "//" + window.location.hostname +  "/";
+    return backend;
+};
