b0231fe28a5a80123b74e8d542ada46db12a7858
Andre <andre@Andres-MacBook-Pro.local>
2018-12-26 11:23:52 +0100

    db config

--- setup/database/updateConfig.sh
+++ setup/database/updateConfig.sh
@@ -3,9 +3,6 @@
 cat /tmp/pg_hba.conf > /var/lib/postgresql/data/pg_hba.conf
 cat /tmp/postgresql.conf > /var/lib/postgresql/data/postgresql.conf
 
-psql -U ${POSTGRES_USER} -d yf_db -c "CREATE USER latya with password 'lat9lat9';"
-#psql -U ${POSTGRES_USER} -d yf_db -c "CREATE USER postgres with password 'asd21fasFD53';"
-psql -U ${POSTGRES_USER} -d yf_db -c "GRANT ALL privileges ON DATABASE yf_db TO latya;"
 psql -U ${POSTGRES_USER} -d yf_db -c "CREATE SCHEMA liquibase;"
 
 pg_restore --verbose -d yf_db -U ${POSTGRES_USER} /tmp/yf_db_dump.sql
