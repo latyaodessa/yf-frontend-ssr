1fa65b0efd0075f085d673ab54f36f4a17c0ac82
Andre <andre@Andres-MacBook-Pro.local>
2018-12-26 11:30:49 +0100

    config

--- setup/deploy.sh
+++ setup/deploy.sh
@@ -4,5 +4,5 @@ STACK=youngfolks
 docker stack rm ${STACK} || true
 docker-compose build
 docker-compose push
-docker rm $(docker ps --all -q -f status=exited)
+docker rm "$(docker ps --all -q -f status=exited)"
 docker stack deploy --compose-file docker-compose.yaml ${STACK}
