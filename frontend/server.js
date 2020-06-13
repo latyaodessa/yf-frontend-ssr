a80dd5a70b9a5c853d393ce9b530bcb32f4602c2
andr2 <andriifedorenko@gmail.com>
2020-06-13 17:30:17 +0200

    ssl

--- frontend/server.js
+++ frontend/server.js
@@ -52,10 +52,7 @@ expr.get('/.well-known/pki-validation/E2614052870FBC9733FFDA272E094F14.txt', (re
 ));
 
 expr.get('/.well-known/pki-validation/8D9C90EF70F260DE79078FEB2477BFCD.txt', (req, res) => (
-    res.status(200).send(`6b6ec086d99b794ec8c3185c989aada79ca437179839ceddde05a54bb56b6302
-    comodoca.com
-    2nYcvwTWvJ0tz6WfsxxF
-    `)
+    res.status(200).sendFile('pki-validation/8D9C90EF70F260DE79078FEB2477BFCD.txt', options)
 ));
 
 
